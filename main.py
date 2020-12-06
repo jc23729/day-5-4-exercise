d9d2f59ad64c8ebb79a37d3f7c66deb4e20bb0aa
Juan Cruz <jc23729@gmail.com>
2020-12-06 16:03:35 +0000

    solution

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #Write your code below this row 👇
 
-for number in range(1, 101):
-  if number % 3 == 0 and number % 5 == 0:
-    print("Fizzbuzz")
-    #Divisible by 3
-  elif number % 3 == 0:
+# for number in range(1, 101):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("Fizzbuzz")
+#     #Divisible by 3
+#   elif number % 3 == 0:
+#     print("Fizz")
+#     #Divisible by 5
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
+
+## so setup range function 
+for number in range (1, 101):
+  #if statement divisible by 3 and 5 
+  if number % 3 == 0 and % 5 == 0:
+    print ("Fizzbuzz")
+  elif number is %3 == 0:
     print("Fizz")
-    #Divisible by 5
-  elif number % 5 == 0:
+  elif number is %5 == 0:
     print("Buzz")
   else:
     print(number)
